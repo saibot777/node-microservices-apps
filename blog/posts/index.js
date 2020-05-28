834355c6ae63ca8a94e08c901dc2d3848f4f7513
saibot777 <stefantrajkovic777@gmail.com>
2020-05-28 11:22:12 +0200

    posts server and routes initiated

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,1 +1,14 @@
-eggegegerg;
+const express = require("express");
+
+const app = express();
+
+const posts = {};
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {});
+
+app.listen(4000, () => {
+  console.log("Listening on post 4000");
+});
